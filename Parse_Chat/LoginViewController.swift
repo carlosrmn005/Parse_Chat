8c716f61b78c322ddf94c5c0438880ccc555c9be
macstudent <macstudent@dhillss-iMac-100.local>
2018-10-05 19:56:44 -0700

    On my way added the alerts for empty fields

---
+++ Parse_Chat/LoginViewController.swift
@@ -0,0 +1,105 @@
+//
+//  LoginViewController.swift
+//  Parse_Chat
+//
+//  Created by macstudent on 10/5/18.
+//  Copyright © 2018 Carlos Roman. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerUser(_ sender: Any)
+    {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
+        
+        if ((usernameField.text?.isEmpty)! || (passwordField.text?.isEmpty)!)
+        {
+            let alertController = UIAlertController(title: "Registration Error", message: "You did not input either a Username or a Password", preferredStyle: .alert)
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+        }
+        
+    }
+    
+    @IBAction func loginUser(_ sender: Any)
+    {
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }
+        
+        if ((usernameField.text?.isEmpty)! || (passwordField.text?.isEmpty)!)
+        {
+            let alertController = UIAlertController(title: "Login Error", message: "You did not input either a Username or a Password", preferredStyle: .alert)
+            // create an OK action
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // handle response here.
+            }
+            // add the OK action to the alert controller
+            alertController.addAction(OKAction)
+            
+            present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+        }
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
